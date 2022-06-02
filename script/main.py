b935ad2c35019d2f563d94a16da76ae75b80c098
Felipe <60896979+obragaa@users.noreply.github.com>
2022-06-02 18:16:16 -0300

    Comparação de preços.

--- script/main.py
+++ script/main.py
@@ -1,33 +1,59 @@
 # This Python file uses the following encoding: utf-8
+def input_value_universal():
+    global reference, price_param
+
+    #distance_param = int(input("Digite uma distância para ser usada como parâmetro (exemplo: 100 km): "))
+    price_param = float(input("Digite um valor para ser comparado entre o carro elétrico e o carro a combustão (exemplo: 7.8 reais): "))
+    print("-----------------------------------------------------------------------------")
+
+    reference = 100
 
 def electric_car_average():
 
-    reference = 100
     try:
+        print("-----------------------------------------------------------------------------")
+
         electric_car_battery = int(input("Digite o valor da bateria (kWh) do carro elétrico: "))
 
         electric_car_autonomy = int(input("Digite a autonomia do veículo (km) do carro elétrico: "))
+        print("-----------------------------------------------------------------------------\n")
     except:
         print("Digite valores coerentes.")
         exit()
 
     average_consumption = round(float((reference * electric_car_battery)/electric_car_autonomy), 2)
 
-    print(average_consumption)
+    car_electric_price_per_reference = round(average_consumption * 0.50, 2)
+
+
+    car_electric_liter_per_reference = round(price_param / car_combustion_liter_price, 2)
+
+    how_much_better_is = round(car_combustion_price_per_reference / car_electric_price_per_reference)
+
+    print(f"Preço gasto com o carro elétrico: "
+          f"R${car_electric_price_per_reference}\n")
+
+    print(f"Preço gasto com o carro a combustão: : "
+          f"R${car_combustion_price_per_reference}\n")
+
+    print(f"O quão econômico o carro elétrico é: "
+          f"{how_much_better_is} vezes menor do que valor do carro a combustão")
+
+
 
 def average_car_combustion():
+    global car_combustion_liter_price, car_combustion_price_per_reference
 
-    reference = 100
+    car_combustion_km_per_liter = int(input("Média de quilômetros percorridos por litro gasto (exemplo: 10km por litro): "))
 
-    car_combustion_liter_per_km = int(input("Média de quilômetros percorridos por litro gasto (exemplo: 1L == 1km): "))
-    car_combustion_liter_price = float(input("Digite o valor da gasolina (exemplo: 4,0): "))
-    car_combustion_liter_per_100km = car_combustion_liter_price * car_combustion_liter_per_km
-
-    print(car_combustion_liter_per_100km)
+    car_combustion_liter_price = float(input("Digite o valor da gasolina (exemplo: 5 por litro): "))
+    car_combustion_price_per_reference = car_combustion_liter_price * car_combustion_km_per_liter
 
 def init():
-    electric_car_average()
+    input_value_universal()
+
     average_car_combustion()
+    electric_car_average()
 
 
 init()
